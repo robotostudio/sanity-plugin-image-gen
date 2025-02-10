690494500f2ba2328bd6c4bf680e1558b9a5b3ae
hrithikroboto <hrithik@robotostudio.com>
2025-02-10 18:36:03 +0530

    fix: Update package version to 0.0.1

--- package.config.ts
+++ package.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   // Remove this block to enable strict export validation
   extract: {
     rules: {
-      'ae-forgotten-export': 'off',
+      // 'ae-forgotten-export': 'off',
       'ae-incompatible-release-tags': 'off',
       'ae-internal-missing-underscore': 'off',
       'ae-missing-release-tag': 'off',
